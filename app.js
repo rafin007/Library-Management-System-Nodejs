38a79b8b09a429900433f28abf5cbb2796d03010
Arefin <rafin.ryan.07@outlook.com>
2018-07-02 02:16:55 +0600

    Admin pages almost done

--- app.js
+++ app.js
@@ -29,6 +29,38 @@ app.get('/admin/home', (req, res) => {
     res.render('admin/home.ejs');
 });
 
+app.get('/admin/books', (req, res)=>{
+    res.render('admin/books.ejs');
+});
+
+app.get('/admin/customers', (req, res)=>{
+    res.render('admin/customers.ejs');
+});
+
+app.get('/admin/profile', (req, res)=>{
+    res.render('admin/profile.ejs');
+});
+
+app.get('/admin/profile/edit', (req, res)=>{
+    res.render('admin/profile-edit.ejs');
+});
+
+app.get('/admin/books/add', (req, res)=> {
+    res.render('admin/books-add.ejs');
+});
+
+app.get('/admin/books/edit/id', (req, res)=> {
+    res.render('admin/books-edit.ejs');
+});
+
+app.get('/admin/books/edit/id', (req, res)=> {
+    res.render('admin/books-edit.ejs');
+});
+
+app.get('/admin/changepass', (req, res)=> {
+    res.render('admin/change-password.ejs');
+});
+
 //server start
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
