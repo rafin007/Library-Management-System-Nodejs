6d28ff9ade7c5119452af2c4ebc76e77c2275c85
Arefin <rafin.ryan.07@outlook.com>
2018-07-04 03:49:06 +0600

    customer module done

--- app.js
+++ app.js
@@ -27,49 +27,57 @@ app.get('/signup', (req, res)=>{
 
 //admin routes
 
-// app.get('/admin/home', (req, res) => {
-//     res.render('admin/home.ejs');
-// });
-//
-// app.get('/admin/books', (req, res)=>{
-//     res.render('admin/books.ejs');
-// });
-//
-// app.get('/admin/customers', (req, res)=>{
-//     res.render('admin/customers.ejs');
-// });
-//
-// app.get('/admin/profile', (req, res)=>{
-//     res.render('admin/profile.ejs');
-// });
-//
-// app.get('/admin/profile/edit', (req, res)=>{
-//     res.render('admin/profile-edit.ejs');
-// });
-//
-// app.get('/admin/books/add', (req, res)=> {
-//     res.render('admin/books-add.ejs');
-// });
-//
-// app.get('/admin/books/edit/id', (req, res)=> {
-//     res.render('admin/books-edit.ejs');
-// });
-//
-// app.get('/admin/books/edit/id', (req, res)=> {
-//     res.render('admin/books-edit.ejs');
-// });
-//
-// app.get('/admin/changepass', (req, res)=> {
-//     res.render('admin/change-password.ejs');
-// });
-//
-// app.get('/admin/customers/add', (req, res)=> {
-//     res.render('admin/customers-add.ejs');
-// });
-//
-// app.get('/admin/customers/edit/id', (req, res)=> {
-//     res.render('admin/customers-edit.ejs');
-// });
+app.get('/admin/home', (req, res) => {
+    res.render('admin/home.ejs');
+});
+
+app.get('/admin/books', (req, res)=>{
+    res.render('admin/books.ejs');
+});
+
+app.get('/admin/customers', (req, res)=>{
+    res.render('admin/customers.ejs');
+});
+
+app.get('/admin/profile', (req, res)=>{
+    res.render('admin/profile.ejs');
+});
+
+app.get('/admin/profile/edit', (req, res)=>{
+    res.render('admin/profile-edit.ejs');
+});
+
+app.get('/admin/books/add', (req, res)=> {
+    res.render('admin/books-add.ejs');
+});
+
+app.get('/admin/books/edit/id', (req, res)=> {
+    res.render('admin/books-edit.ejs');
+});
+
+app.get('/admin/books/edit/id', (req, res)=> {
+    res.render('admin/books-edit.ejs');
+});
+
+app.get('/admin/changepass', (req, res)=> {
+    res.render('admin/change-password.ejs');
+});
+
+app.get('/admin/customers/add', (req, res)=> {
+    res.render('admin/customers-add.ejs');
+});
+
+app.get('/admin/customers/edit/id', (req, res)=> {
+    res.render('admin/customers-edit.ejs');
+});
+
+app.get('/admin/books/issued', (req, res)=> {
+    res.render('admin/books-issued.ejs');
+});
+
+app.get('/admin/customers/profile/id', (req, res)=> {
+    res.render('admin/customer-profile.ejs');
+});
 
 
 
@@ -97,6 +105,10 @@ app.get('/customer/changepass', (req, res)=> {
 
 app.get('/customer/books/borrowed', (req, res)=> {
     res.render('customer/borrowed-books.ejs');
+});
+
+app.get('/customer/books/request', (req, res)=> {
+    res.render('customer/books-request.ejs');
 });
 
 //server start
